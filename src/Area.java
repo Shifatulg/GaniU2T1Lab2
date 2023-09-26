ffd96de9eca6d7b30a550268ef9f8437b0978cb7
BT_1N3_22 <sample@bths.edu>
2023-09-26 09:13:21 -0400

    Rectangle Area and Volume

--- src/Area.java
+++
@@ -1,6 +0,0 @@
-public class Area {
-    public static void main(String[] args) {
-        Circle circle1 = new Circle(7.5);
-        circle1.area();
-    }
-}
--- src/Circle.java
+++
@@ -1,9 +0,0 @@
-public class Circle {
-    private double radius;
-    public Circle(double CircleRadius) {
-        radius = CircleRadius;
-    }
-    public void area() {
-        System.out.print(Math.PI * radius * radius);
-    }
-}
